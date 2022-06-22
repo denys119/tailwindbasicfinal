b4c4264068ad04487d8e850ebd85d4cc415e6c98
Bodea Denis <denis_gabriel94@yahoo.com>
2022-06-22 13:46:43 +0300

    final-code

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"]
+      }
+    },
+    container: {
+      center: true,
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }   
+    },
+  },
+  plugins: [],
+}
